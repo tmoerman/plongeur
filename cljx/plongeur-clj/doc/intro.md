964e6dbf2512d4f43dc79981a8c71753765eb8d0
Thomas Moerman <thomas.moerman@gmail.com>
2016-01-26 22:36:29 +0100

    plongeur CLJ - scaffolding

---
+++ cljx/plongeur-clj/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to plongeur-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cljx/plongeur-clj/test/plongeur_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns plongeur-clj.core-test
+  (:require [clojure.test :refer :all]
+            [plongeur-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
