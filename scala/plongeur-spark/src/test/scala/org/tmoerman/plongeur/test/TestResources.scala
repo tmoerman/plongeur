5c60832f10ad71d15241a1eeffed195b943a6c35
Thomas Moerman <thomas.moerman@gmail.com>
2016-02-14 16:24:50 +0100

    renamed test resources

--- scala/plongeur-spark/src/test/scala/org/tmoerman/plongeur/test/TestResources.scala
+++ scala/plongeur-spark/src/test/scala/org/tmoerman/plongeur/test/TestResources.scala
@@ -1,5 +1,7 @@
 package org.tmoerman.plongeur.test
 
+import org.apache.spark.mllib.linalg.Vectors.dense
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.tmoerman.plongeur.util.RDDFunctions._
 
 import org.apache.commons.lang.StringUtils.trim
@@ -10,19 +12,33 @@ import org.apache.commons.lang.StringUtils.trim
 trait TestResources extends SparkContextSpec {
 
   val wd     = "src/test/resources/data/"
-  val points = wd + "points.csv"
-  val iris   = wd + "iris.csv"
+  val test2DFile    = wd + "test2D.csv"
+  val irisFile      = wd + "iris.csv"
+  val heuristicFile = wd + "heuristic.csv"
 
-  lazy val pointsRDD =
+  lazy val test2DParsed =
     sc
-      .textFile(points)
+      .textFile(test2DFile)
       .map(_.split(",").map(trim))
       .parseCsv{ case Array(a, b, c) => (a.toDouble, b.toDouble, c.toInt) }
 
-  lazy val irisRDD =
+  lazy val test2DLabeledPointsRDD =
+    test2DParsed
+      ._2
+      .zipWithIndex
+      .map{ case ((x, y, _), idx) => LabeledPoint(idx, dense(x, y)) }
+
+  lazy val irisParsed =
     sc
-      .textFile(iris)
+      .textFile(irisFile)
       .map(_.split(",").map(trim))
       .parseCsv{ case Array(a, b, c, d, e) => (a.toDouble, b.toDouble, c.toDouble, d.toDouble, e) }
+
+  lazy val heuristicLabeledPointsRDD =
+    sc
+      .textFile(heuristicFile)
+      .map(_.split(",").map(trim))
+      .zipWithIndex()
+      .map{ case (Array(a, b), idx) => LabeledPoint(idx, dense(a.toDouble, b.toDouble)) }
 
 }
