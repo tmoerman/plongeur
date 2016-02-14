5bce1c96bf25cbc771e769a088b2eace2e3cbd95
Thomas Moerman <thomas.moerman@gmail.com>
2016-02-14 16:25:16 +0100

    Breeze <-> Spark ML vector conversions

---
+++ scala/plongeur-spark/src/main/scala/org/apache/spark/mllib/linalg/VectorConversions.scala
@@ -0,0 +1,25 @@
+package org.apache.spark.mllib.linalg
+
+import org.apache.spark.mllib.linalg.{Vector => MLLibVector}
+import breeze.linalg.{Vector => BreezeVector}
+
+/**
+  * Hackety-hack conversions between Spark MLLib and Breeze vectors.
+  *
+  * @author Thomas Moerman
+  */
+object VectorConversions extends Serializable {
+
+  implicit class MLLibVectorConversion(val vector: MLLibVector) extends AnyVal {
+
+    def toBreeze: BreezeVector[Double] = vector.toBreeze
+
+  }
+
+  implicit class BreezeVectorConversion(val vector: BreezeVector[Double]) extends AnyVal {
+
+    def toMLLib: MLLibVector = Vectors.fromBreeze(vector)
+
+  }
+
+}
