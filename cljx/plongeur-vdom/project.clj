35c65ed618bb44247a644c50df0278f19351be80
Thomas Moerman <thomas.moerman@gmail.com>
2016-03-25 09:34:06 +0100

    cleaned up Leiningen project

--- cljx/plongeur-vdom/project.clj
+++ cljx/plongeur-vdom/project.clj
@@ -1,8 +1,6 @@
 (defproject plongeur-vdom "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :description "Plongeur reactive frontend."
 
   :min-lein-version "2.5.3"
   
@@ -11,71 +9,57 @@
                  [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
 
                  [com.2tothe8th/dominator "0.4.0"]
-                 [jamesmacaulay/zelkova "0.4.0"]
-                 [stch-library/html "0.1.2"]
-                 [org.clojure/core.match "0.2.2"]
+                 [jamesmacaulay/zelkova   "0.4.0"]
+                 [stch-library/html       "0.1.2"]
+                 [org.clojure/core.match  "0.2.2"]
 
-                 [cljsjs/virtual-dom "2.1.1-0"]               ;; figure this out!
+                 ;[cljsjs/virtual-dom      "2.1.1-0"]
                  ]
-  
+
   :plugins [[lein-figwheel "0.5.1"]
-            [lein-cljsbuild "1.1.2" :exclusions [[org.clojure/clojure]]]]
+            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
-  :source-paths ["src"]
+  :source-paths ["src" "script"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "resources/private/js/compiled"
+                                    "target"]
 
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src"]
+  :cljsbuild {:builds {:dev {:source-paths ["src"]
 
-                        ;; If no code is to be run, set :figwheel true for continued automagical reloading
-                        :figwheel {:on-jsload "plongeur-vdom.core/on-js-reload"}
+                             ;; If no code is to be run, set :figwheel true for continued automagical reloading
+                             :figwheel {:on-jsload "plongeur-vdom.core/on-js-reload"}
 
-                        :compiler {:main plongeur-vdom.core
-                                   :externs ["externs/dominator.js"]
-                                   :asset-path "js/compiled/out"
-                                   :output-to "resources/public/js/compiled/plongeur_vdom.js"
-                                   :output-dir "resources/public/js/compiled/out"
-                                   :source-map-timestamp true}}
+                             :compiler {:main plongeur-vdom.core
+                                        :externs ["externs/dominator.js"]
+                                        :asset-path "js/compiled/out"
+                                        :output-to "resources/public/js/compiled/plongeur_vdom.js"
+                                        :output-dir "resources/public/js/compiled/out"
+                                        :source-map-timestamp true}}
 
-                       ;; This next build is an compressed minified build for
-                       ;; production. You can build this with:
-                       ;; lein cljsbuild once min
-                       {:id "min"
-                        :source-paths ["src"]
-                        :compiler {:output-to "resources/public/js/compiled/plongeur_vdom.js"
-                                   :main plongeur-vdom.core
-                                   :externs ["externs/dominator.js"]
-                                   :optimizations :advanced
-                                   :pretty-print false}}]}
+                       ;; compressed minified build for production: `lein cljsbuild once min`
+                       :min {:source-paths ["src"]
+                             :compiler {:output-to "resources/public/js/compiled/plongeur_vdom.js"
+                                        :main plongeur-vdom.core
+                                        :externs ["externs/dominator.js"]
+                                        :optimizations :advanced
+                                        :pretty-print false}}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
+                       :test {:source-paths ["src" "test"]
+                              :compiler {;:externs ["externs/dominator.js"]
+                                         :output-to "resources/private/js/compiled/unit-test.js"
+                                         :optimizations :whitespace
+                                         :preamble ["public/js/vdom.js"]
+                                         :pretty-print true}}}
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+              :test-commands {"unit" ["phantomjs"
+                                      "resources/private/js/compiled/unit-test.js"]}}
 
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  [figwheel-sidecar        "0.5.1"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             })
+  :figwheel {:css-dirs ["resources/public/css"] ;; watch and update CSS
+             :nrepl-port 7888} ;; Start an nREPL server into the running figwheel process
+  )
