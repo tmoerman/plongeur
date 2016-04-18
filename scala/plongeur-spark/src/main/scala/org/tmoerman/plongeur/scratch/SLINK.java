0b5ecf88872e5309ab4aee85b8f43f4055bd8cda
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-18 11:33:01 +0200

    example executable

--- scala/plongeur-spark/src/main/scala/org/tmoerman/plongeur/scratch/SLINK.java
+++ scala/plongeur-spark/src/main/scala/org/tmoerman/plongeur/scratch/SLINK.java
@@ -1,5 +1,12 @@
 package org.tmoerman.plongeur.scratch;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class SLINK {
 
     interface Function2<A, B, C> {
@@ -8,22 +15,24 @@ public class SLINK {
 
     public static final class SLINKClusteringResult {
 
-        public final int[] height;
+        public final double[] height;
         public final int[] parent;
 
-        public SLINKClusteringResult(int[] height, int[] parent) {
+        public SLINKClusteringResult(double[] height, int[] parent) {
             this.height = height;
             this.parent = parent;
         }
 
     }
 
-    public static <A> SLINKClusteringResult slink(A[] data, Function2<A, A, Integer> distance) {
-        final int size = data.length;
+    public static <A> SLINKClusteringResult slink(List<A> data, Function2<A, A, Double> distance) {
+        final int size = data.size();
 
-        final int[] height    = new int[size];
-        final int[] parent    = new int[size];
-        final int[] distanceN = new int[size];
+        // WTF: distances as integers???
+
+        final double[] height    = new double[size];
+        final int[] parent       = new int[size];
+        final double[] distanceN = new double[size];
 
         for (int n = 0; n < size; n++) { // conflation of adding a new point (index) and data structures -> maps could be possible
 
@@ -33,7 +42,7 @@ public class SLINK {
 
             // Step 2
             for (int i = 0; i < n; i++) {
-                distanceN[i] = distance.apply(data[i], data[n]);
+                distanceN[i] = distance.apply(data.get(i), data.get(n));
             }
 
             // Step 3
@@ -57,4 +66,37 @@ public class SLINK {
         }
         return new SLINKClusteringResult(height, parent);
     }
+
+    static class Point {
+        double x, y;
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static Function2<Point, Point, Double> euclidean = new Function2<Point, Point, Double>() {
+        public Double apply(Point p1, Point p2) {
+            return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+        }
+    };
+
+    public static void main(String... args) {
+
+        final double[][] data = {
+                {0, 0}, {0, 1}, {1, 0}, {1, 1},
+                {3, 3}, {3, 4}, {4, 3}, {4, 4}};
+
+        final List<Point> all = new ArrayList<Point>() {{
+            for (double[] point : data) {
+                add(new Point(point[0], point[1]));
+            }
+        }};
+
+        SLINKClusteringResult result = slink(all, euclidean);
+
+
+    }
+
+
 }
