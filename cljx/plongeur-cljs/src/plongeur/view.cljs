0fa100f101d075962a9abe41b49677a5c6a143bc
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-26 14:43:15 +0200

    view uses model APIs

--- cljx/plongeur-cljs/src/plongeur/view.cljs
+++ cljx/plongeur-cljs/src/plongeur/view.cljs
@@ -2,7 +2,8 @@
   (:require [cljs.core.async :as a :refer [>! chan pipe]]
             [quiescent.core :as q :include-macros true :refer-macros [defcomponent]]
             [sablono.core :as s :refer-macros [html]]
-            [foreign.sigma])
+            [foreign.sigma]
+            [plongeur.model :as m])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def node-1 {:id "n1"
@@ -12,34 +13,37 @@
              :size 1
              :color "#FF0"})
 
-(defn graph-id [graph-state] (str "graph-" (:id graph-state)))
+(defn graph-id [id] (str "graph-" id))
 
 (defcomponent Sigma
-  :keyfn (fn [graph-state] (:id graph-state))
+  :keyfn (fn [[id _]] id)
 
-  :on-mount (fn [node graph-state intent-chans]
-              (let [s  (js/sigma. (graph-id graph-state))
+  :on-mount (fn [node [id props] intent-chans]
+
+              (let [s  (js/sigma. (graph-id id))
                     g  (.-graph s)]
-                (.log js/console s)
+                ;; (.log js/console s)
                 (.addNode g (clj->js node-1))
-                (.refresh s)))
+                (.refresh s))
 
-  :on-unmount (fn [node graph-state intent-chans]
+              )
+
+  :on-unmount (fn [node [id props] intent-chans]
                 (.log js/console node))
 
-  [graph-state {:keys [drop-graph] :as intent-chans}]
+  [[id props] {:keys [drop-graph] :as intent-chans}]
   (html [:section {:class "graph-section"}
-         [:button {:on-click #(go (>! drop-graph (:id graph-state)))} "delete " (:id graph-state)]
-         [:div {:id    (graph-id graph-state)
+         [:button {:on-click #(go (>! drop-graph id))} "delete " id]
+         [:div {:id    (graph-id id)
                 :class "graph"}]]))
 
 (defcomponent Root
   [state {:keys [add-graph] :as intent-chans}]
   (html [:div {:id "plongeur-main"}
          [:h1 {} "Bonjour, ici Plongeur"]
-         (for [graph-state (:graphs state)]
-           (Sigma graph-state intent-chans))
-         [:button {:on-click (fn [_] (go (>! add-graph :click)))} "add graph"]]))
+         [:button {:on-click (fn [_] (go (>! add-graph :click)))} "add graph"]
+         (for [graph-state (m/graphs state)]
+           (Sigma graph-state intent-chans))]))
 
 (defn view
   "Returns a stream of view trees, represented as a core.async channel."
