356c4075e3dc216456926b616b71f9e83e42458d
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-26 14:42:57 +0200

    model refactorings

--- cljx/plongeur-cljs/src/plongeur/model.cljs
+++ cljx/plongeur-cljs/src/plongeur/model.cljs
@@ -1,18 +1,39 @@
 (ns plongeur.model
   (:require [cljs.core.async :as a :refer [<! chan to-chan pipe]]
-            [kierros.model :refer [scan-to-states]]))
+            [kierros.model :refer [scan-to-states]]
+            [com.rpl.specter :as s :refer [select transform ALL FIRST]]))
 
-(defn add-graph
-  [_ state]
-  (let [next-id (:seq state)]
-    (-> state
-        (update-in [:seq] inc)
-        (update-in [:graphs] (fn [graphs] (conj graphs {:id next-id}))))))
+; state map
+;      {:seq
+;       :graphs {}
+;       }
 
-(defn drop-graph
-  [id state]
-  (update-in state [:graphs] (fn [graphs] (->> graphs
-                                               (remove #(= id (:id %)))))))
+;; Nothing else but the functions in this namespace touch (or query) the app state directly.
+
+;; queries
+
+(defn graphs [state] (:graphs state))
+
+(defn graph-ids [state] (select [:graphs ALL FIRST] state))
+
+(defn seq-val [state] (:seq state))
+
+;; updates
+
+(defn add-graph [_ state]
+  "Add a graph"
+  (let [graph-id    (seq-val state)
+        graph-props {}]
+    (prn "adding graph " graph-id)
+    (->> state
+         (transform [:seq] inc)
+         (transform [:graphs] #(assoc % graph-id graph-props)))))
+
+(defn drop-graph [id state]
+  "Drop a graph to the app state."
+  (transform [:graphs] #(dissoc % id) state))
+
+;; model machinery
 
 (def intent-handlers
   {:network-calculated (fn [_ state] state)
@@ -23,7 +44,7 @@
 (def default-state
   "Returns a new initial application state."
   {:seq    2
-   :graphs [{:id 1}]})
+   :graphs {1 {}}})
 
 (defn model
   [init-state-chan intent-chans]
--- cljx/plongeur-cljs/test/plongeur/model_test.cljs
+++ cljx/plongeur-cljs/test/plongeur/model_test.cljs
@@ -1,7 +1,24 @@
 (ns plongeur.model-test
-  (:require [cljs.test :refer-macros [deftest is testing run-tests async]]))
+  (:require [cljs.test :refer-macros [deftest is testing run-tests async]]
+            [plongeur.model :as m]))
 
-(deftest fail
-  (is (= 3 3)))
+(deftest graph-ids
+  (let [state {:graphs {1 {:k :v}
+                        2 {:k :v}
+                        3 {:k :v}}}]
+    (is (= (m/graph-ids state) [1 2 3]))))
+
+(deftest add-graph
+  (let [old-state {:seq 1
+                   :graphs {}}
+        new-state (m/add-graph nil old-state)]
+    (is (= new-state {:seq 2 :graphs {1 {}}}))))
+
+(deftest drop-graph
+  (let [old-state {:seq 3
+                   :graphs {1 {}
+                            2 {}}}
+        new-state (m/drop-graph 1 old-state)]
+    (is (= new-state {:seq 3 :graphs {2 {}}}))))
 
 (run-tests)
