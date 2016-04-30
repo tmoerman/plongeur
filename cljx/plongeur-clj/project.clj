f46f050ba5c9be050bca978393b9c713d2551cc2
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-30 21:25:25 +0200

    added some dependencies

--- cljx/plongeur-clj/project.clj
+++ cljx/plongeur-clj/project.clj
@@ -1,4 +1,15 @@
 (defproject plongeur-clj "0.1.0-SNAPSHOT"
-  :description "Plongeur CLJ"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+
+  :description "Plongeur CLJ - the CLJ implementation."
+
+  :url "https://github.com/tmoerman/plongeur"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [com.taoensso/sente "1.8.1"]
+                 [gorillalabs/sparkling "1.2.4"]
+                 
+                 ;; smile
+                 ;; breeze natives ?
+
+                 ])
