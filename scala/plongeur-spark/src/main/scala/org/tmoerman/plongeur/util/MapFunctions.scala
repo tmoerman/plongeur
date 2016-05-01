93b4e4b745e2d84e547747e883f6b373012c8999
Thomas Moerman <thomas.moerman@gmail.com>
2016-05-01 12:56:39 +0200

    added comment

--- scala/plongeur-spark/src/main/scala/org/tmoerman/plongeur/util/MapFunctions.scala
+++ scala/plongeur-spark/src/main/scala/org/tmoerman/plongeur/util/MapFunctions.scala
@@ -11,6 +11,11 @@ object MapFunctions {
 
 class MapFunctions[K, V](val m: Map[K, V]) extends Serializable {
 
+  /**
+    * @param f A function that takes 2 values for the same key and merges them into a new value.
+    * @param other
+    * @return Returns a Map.
+    */
   def merge(f: (V, V) => V)(other: Map[K, V]) =
     m.foldLeft(other){ case (m, (k, v)) => m + (k -> m.get(k).map(f(_, v)).getOrElse(v))}
 
