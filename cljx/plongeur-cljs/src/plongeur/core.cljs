d762778196c88dcc74a326a69e0696353bfff4a8
Thomas Moerman <thomas.moerman@gmail.com>
2016-05-03 20:00:51 +0200

    launching responsibility is moved to system ns.

--- cljx/plongeur-cljs/src/plongeur/core.cljs
+++ cljx/plongeur-cljs/src/plongeur/core.cljs
@@ -5,21 +5,18 @@
             [plongeur.model  :as m]
             [plongeur.view   :as v]
             [plongeur.sigma-driver :as sig]
-            [kierros.quiescent-dom-driver   :as dom]
-            [kierros.sente-websocket-driver :as ws]
-            [kierros.local-storage-driver   :as st]))
+            [kierros.quiescent-dom-driver :as dom]
+            [kierros.sente-client-driver :as ws]
+            [kierros.local-storage-driver :as st]))
 
 (enable-console-print!)
-
-(defn on-js-reload [] (print "on-js-reload"))
 
 (defn plongeur-main
   "Cycle main."
   [{dom-event-chan       :DOM
     sigma-event-chan     :SIGMA
     saved-state-chan     :STORAGE
-    websocket-event-chan :WEB
-    }]
+    websocket-event-chan :WEB}]
   (let [sigma-chan         (chan 10)
         intent-chans       (-> (i/intents)
                                (assoc :sigma-ctrl sigma-chan))
@@ -27,9 +24,12 @@
         states-chan        (m/model saved-state-chan intent-chans)
         states-mult        (mult states-chan)
 
-        pickle-states-chan (->> (sliding-buffer 1) (chan) (tap states-mult))
+        pickle-states-chan (->> (map #(dissoc % :transient))
+                                (chan 10)
+                                (tap states-mult))
 
-        view-states-chan   (->> (chan 10) (tap states-mult))
+        view-states-chan   (->> (chan 10)
+                                (tap states-mult))
 
         request-chan       (chan 10)
 
@@ -39,10 +39,9 @@
      :STORAGE pickle-states-chan
      :WEB     request-chan}))
 
-(cycle/run
-  plongeur-main
-  {:DOM     (dom/make-dom-driver "plongeur-app")
-   :SIGMA   (sig/make-sigma-driver)
-   :STORAGE (st/make-storage-driver "plongeur" m/default-state)
-   ;:WEB    (ws/make-websocket-driver "/chsk")
-   })
+(defn launch-plongeur []
+  (cycle/run plongeur-main
+             {:DOM     (dom/make-dom-driver "plongeur-app")
+              ;:WEB    (ws/make-websocket-driver "/chsk")
+              :SIGMA   (sig/make-sigma-driver)
+              :STORAGE (st/make-storage-driver "plongeur" m/default-state)}))
