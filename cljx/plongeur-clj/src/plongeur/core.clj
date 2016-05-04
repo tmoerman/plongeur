46df0e564f9de5b38bc8a7903aba454203238214
Thomas Moerman <thomas.moerman@gmail.com>
2016-05-04 21:46:17 +0200

    template for core main function

--- cljx/plongeur-clj/src/plongeur/core.clj
+++ cljx/plongeur-clj/src/plongeur/core.clj
@@ -1,5 +1,17 @@
 (ns plongeur.core
-
-  "main function definition"
-  
+  (:require [clojure.core.async :as a :refer [<! >! chan]]
+            [kierros.core :as cycle]
+            [kierros.sente-server-driver :as ws])
   )
+
+(defn plongeur-server-main
+  "Main function cfr. Cycle.js architecture."
+  [{client-request-chan :WEB}]
+  (let [client-push (chan 10)
+
+        ]
+    {:WEB client-push}))
+
+(defn launch-server []
+  (cycle/run plongeur-server-main
+             {:WEB (ws/make-sente-server-driver {})}))
