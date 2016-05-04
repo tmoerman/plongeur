c2c9cbfb1a5924d00a79e64b643f67beb78760d4
Thomas Moerman <thomas.moerman@gmail.com>
2016-05-04 16:07:45 +0200

    workflow/reloaded controls for the plongeur server application

--- cljx/plongeur-clj/dev/dev.clj
+++ cljx/plongeur-clj/dev/dev.clj
@@ -1,2 +1,16 @@
 (ns dev
-  (:require [clojure.tools.namespace.repl :refer [refresh set-refresh-dirs]]))
+  (:require [clojure.tools.namespace.repl :refer [refresh set-refresh-dirs]]
+            [plongeur.system :as plongeur]))
+
+(def system nil)
+
+(defn cycle-off-on []
+  (alter-var-root #'system (fn [shutdown!]
+                             (when-let [f shutdown!] (f))
+                             (plongeur/launch-server))))
+
+(defn stop []
+  (alter-var-root #'system (fn [shutdown!]
+                             (when-let [f shutdown!] (f)))))
+
+(defn reset [] (refresh :after 'dev/cycle-off-on))
