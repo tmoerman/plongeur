ac10b68d5c85cba1aaf8351b784aa23a62936676
Thomas Moerman <thomas.moerman@gmail.com>
2016-05-04 16:07:25 +0200

    referenced by sente

--- cljx/plongeur-clj/project.clj
+++ cljx/plongeur-clj/project.clj
@@ -21,7 +21,7 @@
                  [ring "1.4.0"]
                  [compojure "1.5.0"]
                  [clojurewerkz/route-one "1.2.0"]
-                 [http-kit "2.1.18"]
+                 ;[http-kit "2.1.18"]
                  [com.taoensso/sente "1.8.1"]
                  [com.taoensso/timbre "4.3.1"]
 
