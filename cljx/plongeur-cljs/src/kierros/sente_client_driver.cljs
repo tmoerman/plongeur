bdacabe31c588d50f7cde7a14e237a43d1a6e62e
Thomas Moerman <thomas.moerman@gmail.com>
2016-05-23 21:00:19 +0200

    supports returning a dummy driver

--- cljx/plongeur-cljs/src/kierros/sente_client_driver.cljs
+++ cljx/plongeur-cljs/src/kierros/sente_client_driver.cljs
@@ -8,17 +8,22 @@
 (defn make-sente-client-driver
   "Accepts a path and options.
   Returns a websocket client driver powered by Sente."
-  [path & options]
-  (prn options)
-  (fn [request-chan]
-    (let [{:keys [chsk ch-recv send-fn]} (s/make-channel-socket-client! path {:host "localhost:3000"})
-          server-response-chan           (chan 10)
-          router-shutdown-fn             (s/start-chsk-router! ch-recv #(go (>! server-response-chan %)))]
-      (go-loop []
-               (if-let [request->server (<! request-chan)]
-                 (do (send-fn request->server)
-                     (recur))
-                 (do (some-> chsk s/chsk-disconnect!)
-                     (router-shutdown-fn)
-                     (prn "sente client driver stopped"))))
-      server-response-chan)))
+  [{:keys [path host]} & options]
+  (if (-> options set :dummy)
+    (fn [_]
+      (prn "dummy sente client driver")
+      (chan 10))
+    (fn [request-chan]
+      (let [{:keys [chsk ch-recv send-fn]} (s/make-channel-socket-client! path {:host host})
+            server-response-chan (chan 10)
+            router-shutdown-fn (s/start-chsk-router! ch-recv #(go (>! server-response-chan %)))]
+        (go-loop []
+                 (if-let [request->server (<! request-chan)]
+                   (do (send-fn request->server)
+                       (recur))
+                   (do (some-> chsk s/chsk-disconnect!)
+                       (router-shutdown-fn)
+                       (prn "sente client driver stopped"))))
+        server-response-chan)))
+
+  )
