8dd5685052b45696d44a319953d728e0ccf9285e
Thomas Moerman <thomas.moerman@gmail.com>
2016-06-21 16:41:11 +0200

    added docs

--- cljx/plongeur-clj/dev/user.clj
+++ cljx/plongeur-clj/dev/user.clj
@@ -1,3 +1,10 @@
-(defn dev []
+(defn dev
+  "Usage:
+
+  1. project folder terminal: `lein dive`
+  2. connect nREPL to port 8666 (see project.clj)
+  3. evaluate this namespace in the repl
+  4. evaluate: `(dev)` in the repl"
+  []
   (require 'dev)
   (in-ns 'dev))
