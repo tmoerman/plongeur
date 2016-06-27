768ef116fb841a545ea348354283775533af8c67
Thomas Moerman <thomas.moerman@gmail.com>
2016-06-27 10:18:32 +0200

    added combine-latest + cljs unit tests

--- cljx/plongeur-cljs/src/kierros/async.cljs
+++ cljx/plongeur-cljs/src/kierros/async.cljs
@@ -49,3 +49,29 @@
           timer (do (>! out val) (recur nil))
           in    (recur new-val))))
     out))
+
+(defn combine-latest
+  "Accepts a collection of channels, an optional selector function f and an optional output
+  channel. Returns a channel with the latest values of the input values combined with the
+  selector function. If no selector function is specified, a vector will be returned.
+  The output channel only yields results when at least one value has been offered to each of
+  the input channels. The output channel closes when any of the input channels closes.
+  Inspired by http://rxmarbles.com/#combineLatest"
+  ([chs]   (combine-latest (chan) vector chs))
+  ([f chs] (combine-latest (chan) f chs))
+  ([out f chs]
+   (assert some? chs)
+   (let [ch->idx (->> chs
+                      (map-indexed (fn [i x] [x i]))
+                      (into {}))
+         nil-vec (-> (count chs) (repeat nil) (vec))]
+     (go-loop [state nil-vec]
+              (let [[v ch] (a/alts! chs)]
+                (if (some? v)
+                  (let [idx       (ch->idx ch)
+                        new-state (assoc state idx v)]
+                    (when (every? some? new-state) ; emit when a value is present for every index
+                      (>! out (apply f new-state)))
+                    (recur new-state))
+                  (close! out)))) ; close out when one of the input channels is closed
+     out)))
--- cljx/plongeur-cljs/test/kierros/async_test.cljs
+++ cljx/plongeur-cljs/test/kierros/async_test.cljs
@@ -1,6 +1,6 @@
 (ns kierros.async-test
   (:require [kierros.util :refer [scan]]
-            [kierros.async :refer [chain]]
+            [kierros.async :as ka :refer [chain combine-latest]]
             [cljs.core.async :as a :refer [<! >! close! chan to-chan]]
             [cljs.test :refer-macros [deftest is testing run-tests async]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
@@ -47,5 +47,70 @@
         (<! r)
         (is (= :token @a)))
       (done))))
+
+(testing "combine-latest"
+
+  (deftest trivial-case
+    (async done
+      (let [a (chan 10)
+            combo (combine-latest [a])
+            res (a/into [] combo)]
+        (go
+          (>! a :a1)
+          (>! a :a2)
+          (>! a :a3)
+          (close! a)
+          (is (= (<! res) [[:a1] [:a2] [:a3]])))
+        (done))))
+
+  (deftest two-input-channels
+    (async done
+      (let [a (chan)
+            b (chan)
+            combo (combine-latest [a b])
+            res (a/into [] combo)]
+        (go
+          (>! a :a1)
+          (>! a :a2)
+          (>! b :b1)
+          (>! a :a3)
+          (>! b :b2)
+          (close! b)
+          (is (= (<! res) [[:a2 :b1] [:a3 :b1] [:a3 :b2]])))
+        (done))))
+
+  (deftest with-selector-function
+    (async done
+      (let [a (chan)
+            b (chan)
+            combo (combine-latest + [a b])
+            res (a/into [] combo)]
+        (go
+          (>! a 1)
+          (>! a 2)
+          (>! b 1000)
+          (>! a 3)
+          (>! b 2000)
+          (close! b)
+          (is (= (<! res) [1002 1003 2003])))
+        (done))))
+
+  (deftest input-channel-closes
+    (async done
+      (let [a (chan)
+            b (chan)
+            c (chan)
+            combo (combine-latest [a b c])
+            res (a/into [] combo)]
+        (go
+          (>! a :a1)
+          (>! b :b1)
+          (close! b)
+          (is (= (<! res) [])))
+        (done))))
+
+  ;; deftest hit-me-baby
+
+  )
 
 (run-tests)
