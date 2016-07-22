91650cb67f9d87efaeae524aef1e86d9eeef179a
Thomas Moerman <thomas.moerman@gmail.com>
2016-07-22 15:48:08 +0200

    Lab for scalaz trees ~ in case the memo fix is not sufficient

--- scala/plongeur-spark/src/test/scala/org/tmoerman/lab/TreeLab.scala
+++ scala/plongeur-spark/src/test/scala/org/tmoerman/lab/TreeLab.scala
@@ -2,6 +2,7 @@ package org.tmoerman.lab
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scalaz.Show.showFromToString
 import scalaz._
 import scalaz.syntax._
 
@@ -11,20 +12,38 @@ import scalaz.syntax._
 class TreeLab extends FlatSpec with Matchers with ToTreeOps {
 
   sealed trait Orientation
-  case object ? extends Orientation
   case object L extends Orientation
   case object R extends Orientation
 
-  case class Interval[T](val o: Orientation,
-                         val lo: T,
+  case class Interval[T](val lo: T,
                          val hi: T,
-                         val max:  T) {
+                         val o: Option[Orientation] = None,
+                         val maxSubHi: Option[T] = None,
+                         val minSubLo: Option[T] = None) {
+
+    def isL = o.map(_ == L).getOrElse(false)
+    def isR = o.map(_ == R).getOrElse(false)
+
+    def toL: Interval[T] = copy(o = Some(L))
+    def toR: Interval[T] = copy(o = Some(R))
+
+    def tuple = (lo, hi)
+
+    override def toString =
+      o.map(v => s"$v").getOrElse("") +
+      s"$tuple" +
+      maxSubHi.map(v => s" max: $v").getOrElse("") +
+      minSubLo.map(v => s", min: $v").getOrElse("")
 
   }
 
-  case object Interval {
+  object Interval {
 
-    def apply[T](lo: T, hi: T): Interval[T] = Interval(?, lo, hi, hi)
+    def left[T](lo: T, hi: T, max: Option[T] = None, min: Option[T] = None) =
+      Interval(lo, hi, maxSubHi = max, minSubLo = min, o = Some(L))
+
+    def right[T](lo: T, hi: T, max: Option[T] = None, min: Option[T] = None) =
+      Interval(lo, hi, maxSubHi = max, minSubLo = min, o = Some(R))
 
   }
 
@@ -60,14 +79,14 @@ class TreeLab extends FlatSpec with Matchers with ToTreeOps {
 
           val acc = if (intersects(currentInterval, query)) currentInterval :: Nil else Nil
 
-          val leftOpt  = loc.findChild(_.loc.getLabel.o == L)
-          val rightOpt = loc.findChild(_.loc.getLabel.o == R)
+          val leftOpt  = loc.findChild(_.loc.getLabel.isL)
+          val rightOpt = loc.findChild(_.loc.getLabel.isR)
 
           leftOpt match {
             case None => acc ::: inner(rightOpt)
 
             case Some(left) => {
-              if (left.getLabel.max < query.lo)
+              if (left.getLabel.maxSubHi.map(_ < query.lo).getOrElse(false))
                 acc ::: inner(rightOpt) // skip left tree
               else
                 acc ::: inner(leftOpt) ::: inner(rightOpt)
@@ -83,7 +102,79 @@ class TreeLab extends FlatSpec with Matchers with ToTreeOps {
   }
 
   /**
+    * @param list
+    * @tparam T
+    * @return Returns a triplet of (List, Option, List) where the Option contains the approximately middle element of
+    *         the specified list, whereas the Lists are the flanking elements from the specified List.
+    */
+  def cleave[T](list: List[T]): (List[T], Option[T], List[T]) =
+    list match {
+      case Nil => (Nil, None, Nil)
+      case xs  => xs.splitAt(list.size / 2) match {
+        case (left, x :: right) => (left, Some(x), right)
+        case _                  => throw new Exception()
+      }
+    }
+
+  /**
+    * @param intervals assumed to be sorted.
+    * @tparam T
+    * @return Returns a balanced Interval Search Tree constructed from a list of Intervals.
+    */
+  def create[T](intervals: List[Interval[T]])(implicit num: Numeric[T]): Tree[Interval[T]] = {
+    intervals match {
+      case l => cleave(l) match {
+        case (Nil,  Some(x), Nil) =>
+          x.copy(maxSubHi = Some(x.hi), minSubLo = Some(x.lo)).leaf
+
+        case (left, Some(x), Nil) =>
+          val leftTree = create(left).loc.modifyLabel(_.toL).toTree
+
+          x.node(leftTree)
+            .loc
+            .modifyLabel(self =>
+              self.copy(
+                maxSubHi =
+                  Some(
+                    List(
+                      Some(self.hi),
+                      leftTree.loc.getLabel.maxSubHi).flatten.max),
+                minSubLo =
+                  Some(
+                    List(
+                      Some(self.lo),
+                      leftTree.loc.getLabel.minSubLo).flatten.min)))
+            .toTree
+
+        case (left, Some(x), right) =>
+          val leftTree  = create(left).loc.modifyLabel(_.toL).toTree
+          val rightTree = create(right).loc.modifyLabel(_.toR).toTree
+
+          x.node(leftTree, rightTree)
+            .loc.modifyLabel(self =>
+              self.copy(
+                maxSubHi =
+                  Some(
+                    List(
+                      Some(self.hi),
+                      leftTree.loc.getLabel.maxSubHi,
+                      rightTree.loc.getLabel.maxSubHi).flatten.max),
+                minSubLo =
+                  Some(
+                    List(
+                      Some(self.lo),
+                      leftTree.loc.getLabel.minSubLo,
+                      rightTree.loc.getLabel.minSubLo).flatten.min)))
+            .toTree
+
+        case _ => throw new Exception("dafuq")
+      }
+    }
+  }
+
+  /**
     * Overloaded function to query on singular instance of T
+    *
     * @param query
     * @param tree
     * @param num
@@ -92,13 +183,23 @@ class TreeLab extends FlatSpec with Matchers with ToTreeOps {
     */
   def search[T](query: T, tree: Tree[Interval[T]])(implicit num: Numeric[T]): Iterable[Interval[T]] = search(Interval(query, query), tree)
 
+  import Interval._
+
   val myTree =
-    Interval(?, 17, 19, 24).node(
-      Interval(L, 5, 8, 18).node(
-        Interval(L, 4, 8, 8).leaf,
-        Interval(R, 15, 18, 18).node(
-          Interval(L, 7, 10, 10).leaf)),
-      Interval(R, 21, 24, 24).leaf)
+    Interval(17, 19, maxSubHi = Some(24)).node(
+      left(5, 8, max = Some(18)).node(
+        left(4, 8, max = Some(8)).leaf,
+        right(15, 18, max = Some(18)).node(
+          left(7, 10, max = Some(10)).leaf)),
+      right(21, 24, max = Some(24)).leaf)
+
+  behavior of "Scalaz trees"
+
+  implicit val showInt = showFromToString[Interval[Int]]
+
+  it should "be drawable" in {
+    println(myTree.drawTree)
+  }
 
   behavior of "Interval"
 
@@ -116,17 +217,55 @@ class TreeLab extends FlatSpec with Matchers with ToTreeOps {
   behavior of "search"
 
   it should "find the intersections with a query interval" in {
-    search(Interval(23, 25), myTree) shouldBe Interval(R,21,24,24) :: Nil
+    search(Interval(23, 25), myTree).map(_.tuple) shouldBe (21, 24) :: Nil
 
-    search(Interval(7, 16), myTree) shouldBe Interval(L,5,8,18) :: Interval(L,4,8,8) :: Interval(R,15,18,18) :: Interval(L,7,10,10) :: Nil
+    search(Interval(7, 16), myTree).map(_.tuple) shouldBe (5, 8) :: (4, 8) :: (15, 18) :: (7, 10) :: Nil
 
-    search(Interval(23,23), myTree) shouldBe Interval(R,21,24,24) :: Nil
+    search(Interval(23,23), myTree).map(_.tuple) shouldBe (21, 24) :: Nil
 
-    search(Interval(18, 21), myTree) shouldBe Interval(?,17,19,24) :: Interval(R,15,18,18) :: Interval(R,21,24,24) :: Nil
+    search(Interval(18, 21), myTree).map(_.tuple) shouldBe (17, 19) :: (15, 18) :: (21, 24) :: Nil
   }
 
   it should "find the intersections with a query value" in {
-    search(18, myTree) shouldBe Interval(?,17,19,24) :: Interval(R,15,18,18) :: Nil
+    search(18, myTree).map(_.tuple) shouldBe (17, 19) :: (15, 18) :: Nil
+  }
+
+  behavior of "constructing an Interval Tree"
+
+  val intervals = Range(0, 20, 1).map(i => Interval(i*5, i*5 + 10)).toList
+
+  it should "meh" in {
+    val tree = create(intervals)
+
+    println(tree.drawTree)
+
+    val result = search(19, tree)
+
+    result.size shouldBe 2
+
+    println(result)
+  }
+
+  it should "work naively" in {
+    val naive = intervals match {
+      case head :: rest =>
+        rest.foldLeft(head.leaf.loc){
+          (currentTree: TreeLoc[Interval[Int]], interval: Interval[Int]) => currentTree.insertDownLast(interval.leaf)
+        }
+      case _ => throw new Exception("dafuq")
+    }
+
+    println(naive.toTree.drawTree)
+  }
+
+  behavior of "cleaving lists"
+
+  it should "work correctly" in {
+    cleave(Nil)                 shouldBe (Nil,        None,    Nil)
+    cleave(List(1))             shouldBe (Nil,        Some(1), Nil)
+    cleave(List(1, 2))          shouldBe (List(1),    Some(2), Nil)
+    cleave(List(1, 2, 3, 4))    shouldBe (List(1, 2), Some(3), List(4))
+    cleave(List(1, 2, 3, 4, 5)) shouldBe (List(1, 2), Some(3), List(4, 5))
   }
 
 }
