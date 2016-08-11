8063fbf86ccc66e3be74b612a1e62c08c734f5f5
Thomas Moerman <thomas.moerman@gmail.com>
2016-08-11 14:55:53 +0200

    evolved Colours namespace

--- scala/plongeur-spark/src/test/scala/org/tmoerman/plongeur/tda/ColoursSpec.scala
+++ scala/plongeur-spark/src/test/scala/org/tmoerman/plongeur/tda/ColoursSpec.scala
@@ -1,12 +1,12 @@
 package org.tmoerman.plongeur.tda
 
 import org.scalatest.{Matchers, FlatSpec}
-import org.tmoerman.plongeur.tda.Colouring.pctToBin
+import org.tmoerman.plongeur.tda.Colour.pctToBin
 
 /**
   * @author Thomas Moerman
   */
-class ColouringSpec extends FlatSpec with Matchers {
+class ColoursSpec extends FlatSpec with Matchers {
 
   behavior of "pctToBin"
 
@@ -14,6 +14,12 @@ class ColouringSpec extends FlatSpec with Matchers {
     pctToBin(7, 0d)    shouldBe 0
     pctToBin(7, 0.55d) shouldBe 3
     pctToBin(7, (1d / 7)*6 + 0.1) shouldBe 6
+  }
+
+  behavior of "Brewer"
+
+  it should "correctly parse the palettes.json" in {
+
   }
 
 }
