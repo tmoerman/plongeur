d6d248c658df5ea39a740ce7e8c3bf40935e24c8
Thomas Moerman <thomas.moerman@gmail.com>
2016-10-29 23:17:08 +0200

    additional tests

--- scala/plongeur-spark/src/test/scala/org/tmoerman/cases/l1000/L1000Spec.scala
+++ scala/plongeur-spark/src/test/scala/org/tmoerman/cases/l1000/L1000Spec.scala
@@ -9,9 +9,9 @@ import org.scalatest.{FlatSpec, Matchers}
 import org.tmoerman.plongeur.tda.Distances.LpNormDistance
 import org.tmoerman.plongeur.tda.LSH.LSHParams
 import org.tmoerman.plongeur.tda.Model.{DataPoint, TDAContext, dp}
-import org.tmoerman.plongeur.tda.knn.FastKNN.FastKNNParams
+import org.tmoerman.plongeur.tda.knn.FastKNN_BAK.FastKNNParams
 import org.tmoerman.plongeur.tda.knn.SampledKNN.SampledKNNParams
-import org.tmoerman.plongeur.tda.knn.{FastKNN2, SampledKNN, _}
+import org.tmoerman.plongeur.tda.knn.{FastKNN, SampledKNN, _}
 import org.tmoerman.plongeur.test.SparkContextSpec
 import org.tmoerman.plongeur.util.RDDFunctions._
 import org.tmoerman.plongeur.util.TimeUtils.time
@@ -33,7 +33,7 @@ class L1000Spec extends FlatSpec with SparkContextSpec with Matchers {
 
   val fastKNNParams = {
     val k   = 10
-    val B   = 100
+    val B   = 50
     val sig = 10
     val L   = 1
     val dist = LpNormDistance(0.5)
@@ -47,37 +47,39 @@ class L1000Spec extends FlatSpec with SparkContextSpec with Matchers {
   import lshParams._
 
   it should "compute a series of runs" in {
-    val (pctTotal, sampleSize) = (1.0, Right(0.01))
+    // val (pctTotal, sampleSize) = (1.0, Right(0.01))
     // val (pctTotal, sampleSize) = (0.25, Right(0.10))
-    // val (pctTotal, sampleSize) = (0.1, Right(0.25))
+    val (pctTotal, sampleSize) = (0.1, Right(0.25))
 
     val ctx = TDAContext(sc, if (pctTotal < 1.0) perts.sample(false, pctTotal) else perts)
 
     val sampledKNNParams = SampledKNNParams(k = k, sampleSize = sampleSize, distance = distance)
 
-    val baseLine = SampledKNN.apply(ctx, sampledKNNParams)
+    val baseLine = SampledKNN.apply(ctx, sampledKNNParams).cache
 
     val radius = Some(10.0)
 
     //Stream(1, 5, 10, 15, 20, 25) // , 30, 50) // TODO write this more efficiently with a scan algorithm
-    Option(90)
-    //Stream(10, 30, 60)
+    //Option(90)
+    Stream(10, 30, 60)
       .map(L => fastKNNParams.copy(nrHashTables = L, lshParams = lshParams.copy(radius = radius)))
       .map(p => run(ctx, pctTotal, sampleSize, p, baseLine))
       .foreach(println)
   }
 
-  def run(ctx: TDAContext, pctTotal: Double, sample: Either[Int, Double], fastKNNParams: FastKNNParams, baseLine: kNN_RDD) = {
+  def run(ctx: TDAContext, pctTotal: Double, sample: Either[Int, Double], fastKNNParams: FastKNNParams, baseLine: KNN_RDD) = {
     import fastKNNParams._
     import lshParams._
 
     val ((result, accuracy), wallTime) = time {
-      val rdd = FastKNN2(ctx, fastKNNParams).cache
+      val rdd = FastKNN(ctx, fastKNNParams).cache
 
       (rdd, relativeAccuracy(rdd, baseLine))
     }
 
     val now = DateTime.now
+
+    (distance, k, radius, signatureLength, nrHashTables, blockSize, pctTotal, wallTime.toSeconds, accuracy, sample)
 
     s"| k(NN)=$k | $distance | ${radius.map(v => s"r=$v").getOrElse("N/A")} | sig=$signatureLength | L=$nrHashTables | B=$blockSize | $pctTotal | ${wallTime.toSeconds}s | ${f"$accuracy%1.3f"} | $sample | $now | Spark 2.0.1 | "
   }
